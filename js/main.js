48715c6bd433c33c194f8d137391887276fb64e0
DreamersCreation <dreamerscreation84@gmail.com>
2024-01-31 10:10:09 +0530

    SILDER COMPETED

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+var divisor = document.getElementById("divisor"),
+handle = document.getElementById("handle"),
+slider = document.getElementById("slider");
+
+function moveDivisor() {
+handle.style.left = slider.value + "%";
+divisor.style.width = slider.value + "%";
+}
+
+window.onload = function () {
+moveDivisor();
+};
+
+
 (function ($) {
     "use strict";
 
@@ -83,4 +97,3 @@
 
 
 
- 
