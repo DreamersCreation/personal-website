40a49f0df100abd9b9ec4baac31fdbd5ca02e85d
DreamersCreation <dreamerscreation84@gmail.com>
2024-02-03 17:23:31 +0530

    code update

--- js/main.js
+++ js/main.js
@@ -72,27 +72,48 @@ moveDivisor();
 
 
  // Form script
- const scriptURL = 'https://script.google.com/macros/s/AKfycbxze_pEYSfn90dHIFkCzU0l07IJ_rrtw8BjvUravJ_oZJejbM89iFHY5CjC8UdM66MU8g/exec'
- const form = document.forms['submit-to-google-sheet'];
- const sentmsg = document.getElementById("Success_Msg");
- const unsentmsg = document.getElementById("Unsent_Msg")
 
- form.addEventListener('submit', e => {
-   e.preventDefault()
-   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-     .then(response => {sentmsg.innerHTML = "Message Sent Successfully"
-   setTimeout(function(){
-       sentmsg.innerHTML =""
-   },5000)
-   form.reset()
-})
-     .catch(error => {unsentmsg.innerHTML = "Message Sent Unsuccesfull"
-     setTimeout(function(){
-       unsentmsg.innerHTML =""
-     },5000)
-     form.reset()
- })
- })
+
+ window.addEventListener("DOMContentLoaded", () => {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxze_pEYSfn90dHIFkCzU0l07IJ_rrtw8BjvUravJ_oZJejbM89iFHY5CjC8UdM66MU8g/exec'
+    const form = document.forms['submit-to-google-sheet'];
+    const sentmsg = document.getElementById("Success_Msg");
+    const unsentmsg = document.getElementById("Unsent_Msg")
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then(response => {sentmsg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            sentmsg.innerHTML =""
+        },5000)
+        form.reset()
+     })
+          .catch(error => {unsentmsg.innerHTML = "Message Sent Unsuccesfull"
+          setTimeout(function(){
+            unsentmsg.innerHTML =""
+          },5000)
+          form.reset()
+      })
+      }) 
+});
+
+
+//  form.addEventListener('submit', e => {
+//    e.preventDefault()
+//    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//      .then(response => {sentmsg.innerHTML = "Message Sent Successfully"
+//    setTimeout(function(){
+//        sentmsg.innerHTML =""
+//    },5000)
+//    form.reset()
+// })
+//      .catch(error => {unsentmsg.innerHTML = "Message Sent Unsuccesfull"
+//      setTimeout(function(){
+//        unsentmsg.innerHTML =""
+//      },5000)
+//      form.reset()
+//  })
+//  })
 
 
 
